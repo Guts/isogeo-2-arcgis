14e7e84dd10a6feba3132d6b24c16f28289e35e0
ferdaousse <ferdaousse.chakir@gmail.com>
2015-04-10 17:15:03 +0200

    Update proxy_checker.py
    final

--- modules/proxy_checker.py
+++ modules/proxy_checker.py
@@ -1,5 +1,26 @@
+#!/usr/bin/env python
+from __future__ import unicode_literals
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      cferdaousse
+#
+# Created:     10/04/2015
+# Copyright:   (c) cferdaousse 2015
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+
 import urllib2
 import socket
+# Import system modules and arcpy
+#
+import sys
+import string
+import os
+import arcpy
+execfile("parameters.py")
 
 def is_bad_proxy(pip):
     try:
@@ -20,18 +41,16 @@ def is_bad_proxy(pip):
 def main():
     socket.setdefaulttimeout(120)
 
-    protocole = u"http"
-    prox = u"10.0.4.2"
-    port = u"3128"
-
     # two sample proxy IPs
-    proxyList = ['10.0.4.2:3128', '{0}:{1}'.format(prox, port)]
+    proxyList = ['10.0.4.2:3128', '{0}:{1}'.format(prox, port), '{0}://{1}:{2}@{3}:{4}'.format(protocole, user, password, prox, port)]
 
     for currentProxy in proxyList:
         if is_bad_proxy(currentProxy):
             print "Bad Proxy %s" % (currentProxy)
+            arcpy.AddMessage("Bad Proxy")
         else:
             print "%s is working" % (currentProxy)
+            arcpy.AddMessage("is working")
 
 if __name__ == '__main__':
     main()
