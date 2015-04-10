5048f91743f5a0728bf88c1563cbc2f9a46570e3
marmarih <marih.maryam@gmail.com>
2015-04-10 17:17:56 +0200

    Update match_table.py

--- modules/match_table.py
+++ modules/match_table.py
@@ -3,14 +3,15 @@ import json
 import time
 
 
-#récupération du time
+#récupération du time 
 date = time.clock()
 
 
 
 def remplir(id,idu,path,update):
-        file = open("file.json", "a")
-        line={'date': date,'line':{'ID':id,
-'IDU':idu,'local_Path':path,'Last_update':update}}
-        json.dump(line, file, indent=4)
-        file.close()
+    file = open("file.json", "a") 
+    line={'date': date,'line':{'ID':id, 'IDU':idu,'local_Path':path,'Last_update':update}}
+    json.dump(line, file, indent=4)
+    file.close()
+
+remplir(arcpy.GetParameterAsText(0),arcpy.GetParameterAsText(1),arcpy.GetParameterAsText(2),'test')
